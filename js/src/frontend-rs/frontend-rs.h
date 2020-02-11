78c453569711ed641c497ceb19d4524ad5c6f8ca
Tooru Fujisawa <arai_a@mac.com>
2020-02-11 10:14:15 +0900

    Propagate all plain fields to StencilScript

--- js/src/frontend-rs/frontend-rs.h
+++ js/src/frontend-rs/frontend-rs.h
@@ -15,17 +15,36 @@ struct JsparagusResult {
   CVec<uint8_t> error;
   CVec<uint8_t> bytecode;
   CVec<CVec<uint8_t>> strings;
+  /// Line and column numbers for the first character of source.
+  uintptr_t lineno;
+  uintptr_t column;
+  /// Offset of main entry point from code, after predef'ing prologue.
+  uintptr_t main_offset;
+  /// Fixed frame slots.
+  uint32_t max_fixed_slots;
   /// Maximum stack depth before any instruction.
   ///
   /// This value is a function of `bytecode`: there's only one correct value
   /// for a given script.
   uint32_t maximum_stack_depth;
+  /// Index into the gcthings array of the body scope.
+  uint32_t body_scope_index;
   /// Number of instructions in this script that have IC entries.
   ///
   /// A function of `bytecode`. See `JOF_IC`.
   uint32_t num_ic_entries;
   /// Number of instructions in this script that have JOF_TYPESET.
   uint32_t num_type_sets;
+  /// `See BaseScript::ImmutableFlags`.
+  bool strict;
+  bool bindings_accessed_dynamically;
+  bool has_call_site_obj;
+  bool is_for_eval;
+  bool is_module;
+  bool is_function;
+  bool has_non_syntactic_scope;
+  bool needs_function_environment_objects;
+  bool has_module_goal;
 };
 
 struct JsparagusCompileOptions {
